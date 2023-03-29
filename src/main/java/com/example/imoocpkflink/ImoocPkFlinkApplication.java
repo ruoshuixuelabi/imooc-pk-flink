83e7a5cffa576ed8b05263f22d97331133563557
mzk <524991368@qq.com>
2023-03-29 15:16:31 +0800

    Initial commit

---
+++ src/main/java/com/example/imoocpkflink/ImoocPkFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.imoocpkflink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocPkFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocPkFlinkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/imoocpkflink/ImoocPkFlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imoocpkflink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocPkFlinkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
